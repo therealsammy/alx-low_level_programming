cc96a168080161019055a067b55b3731c33f9165
Samuel Ohiri <therealsammy18x@gmail.com>
2022-09-08 02:57:29 -0700

    Update file 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Print exactly "with proper grammar, but the outcome is a piece of art,"
+ * main - Print exactly with proper grammar, but the outcome is a piece of art,
  * but the outcome is a piece of art,
  * Return: 0 if executed without error, non-zero otherwise
  */
