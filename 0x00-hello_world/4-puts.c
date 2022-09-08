855781f02ac6fd391892f03cff96668ec93a3d95
Samuel Ohiri <therealsammy18x@gmail.com>
2022-09-08 03:00:47 -0700

    Update file 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /*
- *Print exactly "Programming is like building a multilingual puzzle
- *Return  0 if executed properly
+ * main - Print exactly "Programming is like building a multilingual puzzle
+ * Return: 0 if executed properly
  */
 
-int main(void) {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return 0;
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
