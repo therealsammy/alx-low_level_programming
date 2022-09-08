8078cb48868f6c049b1dfa29f96e8dd423f86675
Samuel Ohiri <therealsammy18x@gmail.com>
2022-09-08 02:54:43 -0700

    Update file 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-/*
+/**
  * main - Print exactly "with proper grammar, but the outcome is a piece of art,"
- * Return 0 if executed without error
+ * but the outcome is a piece of art,
+ * Return: 0 if executed without error, non-zero otherwise
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
