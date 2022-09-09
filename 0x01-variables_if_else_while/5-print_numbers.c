a3fdae00370fdbd73d43625cb852c83ac17bfca0
Samuel Ohiri <therealsammy18x@gmail.com>
2022-09-09 07:52:29 -0700

    File 5 added

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - write all the digits in base 10
+ * Return: 0
+ */
+
+int main(void)
+{
+	n = 10;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+
+}
