7e366c56bac41a51ce49093a6cb83558bc56b853
Samuel Ohiri <therealsammy18x@gmail.com>
2022-09-14 02:26:42 -0700

    Files added

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints integers
+ * if integers are negative or positive
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+
+}
