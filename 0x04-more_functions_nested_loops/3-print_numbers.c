40e27aa05e547ee2c29c8ea20510f750c61ef53e
Samuel Ohiri <therealsammy18x@gmail.com>
2022-09-15 07:37:28 -0700

    File 3 added

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,8 +5,6 @@
  * using _putchar
  * can only use _putchar twice
  *
- * @i: variable to be incremented
- *
  * Return: 0 to 9
  */
 
@@ -18,6 +16,6 @@ void print_numbers(void)
 	{
 		_putchar(i + '0');
 	}
-	
+
 	_putchar('\n');
 }
