e09672fb678f6c0936b8152457e15e191300b053
Samuel Ohiri <therealsammy18x@gmail.com>
2022-09-15 13:16:06 -0700

    Update Fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,23 +18,22 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
 		}
-		if (i == 100)
+		if (i != 100)
 		{
-			continue;
 			printf(" ");
 		}
 	}
