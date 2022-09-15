b4ec4ff83255627437de202a649f5b5b729a78b2
Samuel Ohiri <therealsammy18x@gmail.com>
2022-09-15 13:09:25 -0700

    Update Fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,23 +14,23 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("Fizz");
+			printf("FizzBuzz ");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz");
+			printf("Buzz ");
 		}
-		else if ((i % 3 == 0) && (i % 5 == 0))
+		else if (i % 3 == 0)
 		{
-			printf("FizzBuzz");
+			printf("Fizz ");
 		}
 		else
 		{
-			printf("%d", i);
+			printf("%d ", i);
 		}
 		if (i == 100)
 		{
