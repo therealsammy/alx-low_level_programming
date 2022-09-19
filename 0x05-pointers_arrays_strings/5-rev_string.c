ad4bdfb52168a442bcc09e1b10b59bb40549d3b9
Samuel Ohiri <therealsammy18x@gmail.com>
2022-09-19 11:45:20 -0700

    5 Reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,21 +6,6 @@
 
 void rev_string(char *s)
 {
-	/*char map;
-	int i, j, n;
-
-	for (j = 0; s[j] != '\0'; j++)
-		;
-
-	n = j - 1;
-
-	for (i = 0; i < n / 2; i++)
-	{
-		map = s[i];
-		s[i] = s[n];
-		s[n--] = map;
-	}*/
-
 	int i, c, l;
 	char h;
 
