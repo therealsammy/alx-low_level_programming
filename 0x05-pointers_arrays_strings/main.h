bc697ef74794a5d94042904614e49fd3463b108d
Samuel Ohiri <therealsammy18x@gmail.com>
2022-09-19 12:28:12 -0700

    Files 8 and 9

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,5 +9,7 @@ void rev_string(char *s);
 void print_rev(char *s);
 void puts2(char *str);
 void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 
 #endif /*MAIN_H*/
