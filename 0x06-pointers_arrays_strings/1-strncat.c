67ba56e3d6a361d2632626e2059c2123a782fcdf
Samuel Ohiri <therealsammy18x@gmail.com>
2022-09-21 07:51:44 -0700

    0x06-pointers_arrays_strings Readme, gitignore and files 0, 1, and 2 and header file

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenate two strings
+ * @dest: string
+ * @src: string
+ * @n: number of elements to concatenate in
+ * Return: pointer to resulting `dest`
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+
+	for (j = 0; src[j] != '\0' && n > 0; j++, n--, i++)
+	{
+		dest[i] = src[j];
+	}
+
+	return (dest);
+}
