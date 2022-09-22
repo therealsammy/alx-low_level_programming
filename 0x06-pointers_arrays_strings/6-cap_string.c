f8622e0d100987822b44a7d0d1a9dee74de929dd
Samuel Ohiri <therealsammy18x@gmail.com>
2022-09-22 04:12:10 -0700

    File 6 updated

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,27 +1,26 @@
 #include "main.h"
 
 /**
- * cap_string - capitalize all words of a string
+ * cap_string - capitalize all words of a strin
  * @str: string
  * Return: `str`
  */
 
 char *cap_string(char *str)
 {
-	int i, n;
-	int control;
-	char separators[] = ",;.!?(){}\n\t\" ";
+	int i, n control;
+	char separator = ",;.!?(){}\n\t\" ";
 
 	for (i = 0, control = 0; str[i] != '\0'; i++)
 	{
-		if (str[0] >= 'a' && str[0] <= 'z')
+		if (str[i] >= 'a' && str <= 'z')
 		{
 			control = 1;
 		}
 
-		for (n = 0; separators[n] != '\0'; n++)
+		for (n = 0; separator != '\0'; n++)
 		{
-			if (separators[n] == str[i])
+			if (separator[n] == str[i])
 			{
 				control = 1;
 			}
@@ -29,14 +28,14 @@ char *cap_string(char *str)
 
 		if (control)
 		{
-			if str[i] >= 'a' && str[i] <= 'z')
+			if (str[i] >= 'a' && str[i] <= 'z')
 			{
 				str[i] -= 32;
 				control = 0;
 			}
 			else if (str[i] >= 'A' && str[i] <= 'Z')
 			{
-				control = 1;
+				control = 0;
 			}
 			else if (str[i] > 47 && str[i] < 58)
 			{
@@ -44,6 +43,6 @@ char *cap_string(char *str)
 			}
 		}
 	}
-
 	return (str);
+
 }
