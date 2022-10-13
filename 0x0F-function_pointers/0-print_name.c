d66f56fa92d2651946725ed49d5ad1fd0682533f
Samuel Ohiri <therealsammy18x@gmail.com>
2022-10-12 17:00:30 -0700

    First commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name- this functions prints a name
+ * @name: string to print.
+ * @f: this a function pointer, that recieves a string.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	(*f)(name);
+}
